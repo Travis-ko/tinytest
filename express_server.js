941b730703b192aed03f1affb7ad77ad68c10e51
Travis-ko <traviskothlow@gmail.com>
2017-10-10 20:09:50 +0000

    initial commit for tiny app

---
+++ express_server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+//sets view engine to ejs
+app.set('view engine', 'ejs');
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
