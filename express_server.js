60c74e6ae00492e2ac47d3949d352604ccbbcd81
Travis-ko <traviskothlow@gmail.com>
2017-10-12 20:41:40 +0000

    commit with cookies/login/logout features

--- express_server.js
+++ express_server.js
@@ -1,29 +1,43 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 8080; // default port 8080
+var cookieParser = require('cookie-parser');
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('sbdkjbasjkbdjkasdhvhwd'));
 //sets view engine to ejs
 app.set('view engine', 'ejs');
+var urlDatabase = {};
+
+app.use((req, res, next) => {
+    res.locals.username = req.cookies.username;
+    console.log(req.cookies);
+    return next();
+});
 
 function generateRandomString() {
     temp = Math.random().toString(36).replace(/[^a-z]+/g, '');
     return temp;
 }
-//{urls[url].shortURL}
-var urlDatabase = {
-    // "b2xVn2": "http://www.lighthouselabs.ca",
-    // "9sm5xK": "http://www.google.com"
-};
 
 app.get("/urls/new", (req, res) => {
     res.render("urls_new");
 });
 
+app.post("/login", (req, res) => {
+    res.cookie('username', req.body.username);
+    res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+   res.clearCookie(req.body.username);
+   res.redirect("/urls");
+ });
+
 app.post("/urls", (req, res) => {
     var temp = generateRandomString();
     urlDatabase[temp] = req.body.longURL;
-    console.log(urlDatabase); // debug statement to see POST parameters
+    //console.log(urlDatabase); // debug statement to see POST parameters
     res.redirect("http://localhost:8080/urls/" + temp); // Respond with 'Ok' (we will replace this)
 });
 
@@ -47,13 +61,13 @@ app.get("/urls/:id", (req, res) => {
 
 app.post("/urls/:id", (req, res) => {
     urlDatabase[req.params.id] = req.body.longURL;
-    console.log(req.params.id);
+    //console.log(req.params.id);
     res.redirect("/urls");
 });
 
 app.get("/u/:shortURL", (req, res) => {
     let longURL = urlDatabase[req.params.shortURL];
-    console.log(longURL);
+    //console.log(longURL);
     res.redirect(302, longURL);
 });
 
